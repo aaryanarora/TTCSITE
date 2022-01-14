6cfc67ca0533b55bc022c12496dd88c7776661cb
Aaryan <Aaryanarora1871@gmail.com>
2022-01-14 19:06:02 +0530

    added alloy tab

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/service",function(req,res){
 
     res.sendFile(__dirname + "/card.html");
 })
+app.get("/alloy",function(req,res){
+
+    res.sendFile(__dirname + "/alloy.html");
+})
 
 
 app.listen(process.env.PORT || 3000,function(){
---
+++ public/alloy.js
@@ -0,0 +1,836 @@
+var data = [
+	{
+		"Size": "145/80R13",
+		"Brand": "BRIDGESTONE",
+		"Model": "B290",
+		"price": "NONE",
+		"image": "images/145-80-13_BJS.jpeg"
+	},
+	{
+		"Size": "165/65R14",
+		"Brand": "GOODYEAR",
+		"Model": "DP SERIES",
+		"price": "NONE",
+		"image": "images/165-65-14_GY.jpeg"
+	},
+	{
+		"Size": "165/70R14",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/165-70-14_YOKO.jpeg"
+	},
+	{
+		"Size": "175/65R15",
+		"Brand": "MICHELIN",
+		"Model": "ENERGY XM2",
+		"price": "NONE",
+		"image": "images/175-65-15_MIC.jpeg"
+	},
+	{
+		"Size": "185/60R15",
+		"Brand": "MICHELIN",
+		"Model": "ENERGY XM2+",
+		"price": "NONE",
+		"image": "images/185-60-15_MIC.jpeg"
+	},
+	{
+		"Size": "185/65R15",
+		"Brand": "GOODYEAR",
+		"Model": "Assurance TripleMax",
+		"price": "NONE",
+		"image": "images/185-65-15_GY.jpeg"
+	},
+	{
+		"Size": "195/60R15",
+		"Brand": "YOKOHAMA",
+		"Model": "S-DRIVE",
+		"price": "NONE",
+		"image": "images/195-60-15_YOKO.jpeg"
+	},
+	{
+		"Size": "195/65R15",
+		"Brand": "YOKOHAMA",
+		"Model": "BLUEARTH-GT",
+		"price": "NONE",
+		"image": "images/195-65-15_YOKO.jpeg"
+	},
+	{
+		"Size": "205/55R15",
+		"Brand": "YOKOHAMA",
+		"Model": "S-DRIVE",
+		"price": "NONE",
+		"image": "images/205-55-15_YOKO.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "BRIDGESTONE",
+		"Model": "B250",
+		"price": "NONE",
+		"image": "images/195-55-16_BJS.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "GOODYEAR",
+		"Model": "Assurance TripleMax",
+		"price": "NONE",
+		"image": "images/195-55-16_GY.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/195-55-16_MIC.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/195-55-16_YOKO.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "YOKOHAMA",
+		"Model": "S-DRIVE",
+		"price": "NONE",
+		"image": "images/195-55-16_YOKOS.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "HANKOOK",
+		"Model": "KINERGY EX",
+		"price": "NONE",
+		"image": "images/195-55-16_HANK.jpeg"
+	},
+	{
+		"Size": "195/55R16",
+		"Brand": "PIRELLI",
+		"Model": "CINTURATO P1 VERDE",
+		"price": "NONE",
+		"image": "images/195-55-16_PIR.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "PIRELLI",
+		"Model": "Cinturato All Season S-I",
+		"price": "NONE",
+		"image": "images/205-55-16_PIR.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "PIRELLI",
+		"Model": "P6 CINTURATO",
+		"price": "NONE",
+		"image": "images/205-55-16_PIR_P6.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "BRIDGESTONE",
+		"Model": "ECOPIA EP150",
+		"price": "NONE",
+		"image": "images/205-55-16_BJS.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "BRIDGESTONE",
+		"Model": "ECOPIA EP300",
+		"price": "NONE",
+		"image": "images/205-55-16_BJS_ECO.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/205-55-16_YOKO.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "GOODYEAR",
+		"Model": "Assurance TripleMax 2",
+		"price": "NONE",
+		"image": "images/205-55-16_GY.jpeg"
+	},
+	{
+		"Size": "205/55R16",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/205-55-16_YOKO.jpeg"
+	},
+	{
+		"Size": "205/65R16",
+		"Brand": "PIRELLI",
+		"Model": "CINTURATO",
+		"price": "NONE",
+		"image": "images/205-65-16_PIR.jpeg"
+	},
+	{
+		"Size": "205/65R16",
+		"Brand": "GOODYEAR",
+		"Model": "Assurance TripleMax",
+		"price": "NONE",
+		"image": "images/205-65-16_GY.jpeg"
+	},
+	{
+		"Size": "205/65R16",
+		"Brand": "BRIDGESTONE",
+		"Model": "B390",
+		"price": "NONE",
+		"image": "images/205-65-16_BJS.jpeg"
+	},
+	{
+		"Size": "205/65R16",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/205-65-16_YOKO.jpeg"
+	},
+	{
+		"Size": "205/65R16",
+		"Brand": "MICHELIN",
+		"Model": "ENERGY XM2+",
+		"price": "NONE",
+		"image": "images/205-65-16_MIC.jpeg"
+	},
+	{
+		"Size": "215/60R16",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/215-60-16_YOKO.jpeg"
+	},
+	{
+		"Size": "225/60R16",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/225-60-16_MIC.jpeg"
+	},
+	{
+		"Size": "225/60R16",
+		"Brand": "ACCELERA",
+		"Model": "ECO PLUSH",
+		"price": "NONE",
+		"image": "images/225-60-16_ACC.jpeg"
+	},
+	{
+		"Size": "215/60R17",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/215-60-17_MIC.jpeg"
+	},
+	{
+		"Size": "225/45R17",
+		"Brand": "PIRELLI",
+		"Model": "CINTURATO P7",
+		"price": "NONE",
+		"image": "images/225-45-17_PIR.jpeg"
+	},
+	{
+		"Size": "225/45R17",
+		"Brand": "PIRELLI",
+		"Model": "CINTURATO P7 (MO)",
+		"price": "NONE",
+		"image": "images/225-45-17_PIR_MO.jpeg"
+	},
+	{
+		"Size": "225/45R17",
+		"Brand": "YOKOHAMA",
+		"Model": "BLUEARTH-GT",
+		"price": "NONE",
+		"image": "images/225-45-17_YOKO.jpeg"
+	},
+	{
+		"Size": "225/45R17",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/225-45-17_MIC.jpeg"
+	},
+	{
+		"Size": "225/45R17",
+		"Brand": "CONTINENTAL",
+		"Model": "ContiSportContact 5",
+		"price": "NONE",
+		"image": "images/225-45-17_CONTI.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/225-50-17_MIC.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "BRIDGESTONE",
+		"Model": "TURANZA T005",
+		"price": "NONE",
+		"image": "images/225-50-17_BJS.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "BRIDGESTONE",
+		"Model": "POTENZA RE050",
+		"price": "NONE",
+		"image": "images/225-50-17_BJS_POT.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "FALKEN",
+		"Model": "ZIEX ZE914",
+		"price": "NONE",
+		"image": "images/225-50-17_FALK.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "GOODYEAR",
+		"Model": "EFFICIENT GRIP",
+		"price": "NONE",
+		"image": "images/225-50-17_GY.jpeg"
+	},
+	{
+		"Size": "225/50R17",
+		"Brand": "PIRELLI",
+		"Model": "P7 CINTURATO",
+		"price": "NONE",
+		"image": "images/225-50-17_PIR.jpeg"
+	},
+	{
+		"Size": "225/55R17",
+		"Brand": "ACCELERA",
+		"Model": "PHI-R",
+		"price": "NONE",
+		"image": "images/225-55-17_ACC.jpeg"
+	},
+	{
+		"Size": "225/60R17",
+		"Brand": "PIRELLI",
+		"Model": "P4",
+		"price": "NONE",
+		"image": "images/225-60-17_PIR.jpeg"
+	},
+	{
+		"Size": "225/60R17",
+		"Brand": "PIRELLI",
+		"Model": "P7 CINTURATO (*)",
+		"price": "NONE",
+		"image": "images/225-60-17_PIR_CINT.jpeg"
+	},
+	{
+		"Size": "225/60R17",
+		"Brand": "YOKOHAMA",
+		"Model": "EARTH-1",
+		"price": "NONE",
+		"image": "images/225-60-17_YOKO.jpeg"
+	},
+	{
+		"Size": "225/60R17",
+		"Brand": "DUNLOP",
+		"Model": "GRANDTREK ST20",
+		"price": "NONE",
+		"image": "images/225-60-17_DUN.jpeg"
+	},
+	{
+		"Size": "225/60R17",
+		"Brand": "FIRESTONE",
+		"Model": "LE-02",
+		"price": "NONE",
+		"image": "images/225-60-17_FS.jpeg"
+	},
+	{
+		"Size": "245/45R17",
+		"Brand": "GOODYEAR",
+		"Model": "EFFICIENT GRIP",
+		"price": "NONE",
+		"image": "images/245-45-17_GY.jpeg"
+	},
+	{
+		"Size": "245/45R17",
+		"Brand": "CONTINENTAL",
+		"Model": "ContiSportContact 5",
+		"price": "NONE",
+		"image": "images/245-45-17_CONTI.jpeg"
+	},
+	{
+		"Size": "245/45R17",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/245-45-17_MIC.jpeg"
+	},
+	{
+		"Size": "245/45R17",
+		"Brand": "ACCELERA",
+		"Model": "PHI",
+		"price": "NONE",
+		"image": "images/245-45-17_ACC.jpeg"
+	},
+	{
+		"Size": "265/65R17",
+		"Brand": "PIRELLI",
+		"Model": "S-ATR",
+		"price": "NONE",
+		"image": "images/265-65-17_PIR.jpeg"
+	},
+	{
+		"Size": "215/55R18",
+		"Brand": "GOODYEAR",
+		"Model": "EfficientGrip",
+		"price": "NONE",
+		"image": "images/215-55-18_GY.jpeg"
+	},
+	{
+		"Size": "225/45R18",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO PZ4",
+		"price": "NONE",
+		"image": "images/225-45-18_PIR.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "PIRELLI",
+		"Model": "CINTURATO P7 (*)",
+		"price": "NONE",
+		"image": "images/225-50-18_PIR.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "PIRELLI",
+		"Model": "P7 CINTURATO (*)",
+		"price": "NONE",
+		"image": "images/225-50-18_PIR_CINT.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/225-50-18_MIC.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "POTENZA S001 RFT",
+		"price": "NONE",
+		"image": "images/225-50-18_BJS.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "FALKEN",
+		"Model": "ZIEX ZE912",
+		"price": "NONE",
+		"image": "images/225-50-18_FALK.jpeg"
+	},
+	{
+		"Size": "225/50R18",
+		"Brand": "ACCELERA",
+		"Model": "PHI-R",
+		"price": "NONE",
+		"image": "images/225-50-18_ACC.jpeg"
+	},
+	{
+		"Size": "225/55R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "TURANZA T001",
+		"price": "NONE",
+		"image": "images/225-55-18_BJS.jpeg"
+	},
+	{
+		"Size": "235/50R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "DUELER H/P SPORT",
+		"price": "NONE",
+		"image": "images/235-50-18_BJS.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "PIRELLI",
+		"Model": "SCORPION VERDE S-I",
+		"price": "NONE",
+		"image": "images/235-55-18_PIR.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "PIRELLI",
+		"Model": "SCORPION VERDE (MO)",
+		"price": "NONE",
+		"image": "images/235-55-18_PIR_MO.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "ALENZA",
+		"price": "NONE",
+		"image": "images/235-55-18_BJS.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "HANKOOK",
+		"Model": "VENTUS S1 SUV ",
+		"price": "NONE",
+		"image": "images/235-55-18_HAN.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "MICHELIN",
+		"Model": "PREMIERE LTX",
+		"price": "NONE",
+		"image": "images/235-55-18_MIC.jpeg"
+	},
+	{
+		"Size": "235/55R18",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/235-55-18_ACC.jpeg"
+	},
+	{
+		"Size": "235/60R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "ECOPIA EP850",
+		"price": "NONE",
+		"image": "images/235-60-18_BJS.jpeg"
+	},
+	{
+		"Size": "235/60R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "DUELER H/P SPORT",
+		"price": "NONE",
+		"image": "images/235-60-18_BJS.jpeg"
+	},
+	{
+		"Size": "235/60R18",
+		"Brand": "MICHELIN",
+		"Model": "LATITUDE SPORT 3",
+		"price": "NONE",
+		"image": "images/235-60-18_MIC.jpeg"
+	},
+	{
+		"Size": "245/40R18",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO PZ4",
+		"price": "NONE",
+		"image": "images/245-40-18_PIR.jpeg"
+	},
+	{
+		"Size": "245/40R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "TURANZA T005",
+		"price": "NONE",
+		"image": "images/245-40-18_BJS.jpeg"
+	},
+	{
+		"Size": "245/45R18",
+		"Brand": "PIRELLI",
+		"Model": "P7 CINTURATO (*)(MOE)",
+		"price": "NONE",
+		"image": "images/245-45-18_PIR_CINT.jpeg"
+	},
+	{
+		"Size": "245/45R18",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO PZ4 (*)",
+		"price": "NONE",
+		"image": "images/245-45-18_PIR.jpeg"
+	},
+	{
+		"Size": "245/45R18",
+		"Brand": "BRIDGESTONE",
+		"Model": "T05A",
+		"price": "NONE",
+		"image": "images/245-45-18_BJS.jpeg"
+	},
+	{
+		"Size": "245/45R18",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY 4ST",
+		"price": "NONE",
+		"image": "images/245-45-18_MIC.jpeg"
+	},
+	{
+		"Size": "255/55R18",
+		"Brand": "MICHELIN",
+		"Model": "PILOT SPORT 4 SUV",
+		"price": "NONE",
+		"image": "images/255-55-18_MIC.jpeg"
+	},
+	{
+		"Size": "255/60R18",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/255-60-18_ACC.jpeg"
+	},
+	{
+		"Size": "265/60R18",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/265-60-18_ACC.jpeg"
+	},
+	{
+		"Size": "265/60R18",
+		"Brand": "MRF",
+		"Model": "WANDERER",
+		"price": "NONE",
+		"image": "images/265-60-18_MRF.jpeg"
+	},
+	{
+		"Size": "285/60R18",
+		"Brand": "MICHELIN",
+		"Model": "PRIMACY SUV",
+		"price": "NONE",
+		"image": "images/285-60-18_MIC.jpeg"
+	},
+	{
+		"Size": "285/60R18",
+		"Brand": "MAXXIS",
+		"Model": "BRAVO AT-980",
+		"price": "NONE",
+		"image": "images/285-60-18_MAXXIS.jpeg"
+	},
+	{
+		"Size": "235/50R19",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO PZ4",
+		"price": "NONE",
+		"image": "images/235-50-19_PIR.jpeg"
+	},
+	{
+		"Size": "235/55R19",
+		"Brand": "YOKOHAMA",
+		"Model": "GEOLANDER",
+		"price": "NONE",
+		"image": "images/235-55-19_YOKO.jpeg"
+	},
+	{
+		"Size": "235/55R19",
+		"Brand": "TRIANGLE",
+		"Model": "ADVANTEX SUV",
+		"price": "NONE",
+		"image": "images/235-55-19_TRI.jpeg"
+	},
+	{
+		"Size": "245/45R19",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO",
+		"price": "NONE",
+		"image": "images/245-45-19_PIR.jpeg"
+	},
+	{
+		"Size": "255/50R19",
+		"Brand": "MICHELIN",
+		"Model": "LATITUDE SPORT 3",
+		"price": "NONE",
+		"image": "images/255-50-19_MIC.jpeg"
+	},
+	{
+		"Size": "255/50R19",
+		"Brand": "TRIANGLE",
+		"Model": "SportX",
+		"price": "NONE",
+		"image": "images/255-50-19_triangle.jpeg"
+	},
+	{
+		"Size": "255/50R19",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/255-50-19_ACC.jpeg"
+	},
+	{
+		"Size": "275/40R19",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO (MO)",
+		"price": "NONE",
+		"image": "images/275-40-19_PIR.jpeg"
+	},
+	{
+		"Size": "275/40R19",
+		"Brand": "MICHELIN",
+		"Model": "PILOT SPORT 4",
+		"price": "NONE",
+		"image": "images/275-40-19_MIC.jpeg"
+	},
+	{
+		"Size": "275/55R19",
+		"Brand": "MICHELIN",
+		"Model": "LATITUDE SPORT",
+		"price": "NONE",
+		"image": "images/275-55-19_MIC.jpeg"
+	},
+	{
+		"Size": "285/45R19",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/285-45-19_ACC.jpeg"
+	},
+	{
+		"Size": "255/50R20",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/255-50-20_ACC.jpeg"
+	},
+	{
+		"Size": "275/45R20",
+		"Brand": "PIRELLI",
+		"Model": "SCORPION VERDE A/S",
+		"price": "NONE",
+		"image": "images/275-45-20_PIR.jpeg"
+	},
+	{
+		"Size": "275/45R20",
+		"Brand": "BRIDGESTONE",
+		"Model": "ALENZA 001 RFT",
+		"price": "NONE",
+		"image": "images/275-45-20_BJS.jpeg"
+	},
+	{
+		"Size": "275/45R20",
+		"Brand": "TRIANGLE",
+		"Model": "SporteX",
+		"price": "NONE",
+		"image": "images/275-45-20_triangle.jpeg"
+	},
+	{
+		"Size": "305/40R20",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO (NO)",
+		"price": "NONE",
+		"image": "images/305-40-20_PIR.jpeg"
+	},
+	{
+		"Size": "275/45R21",
+		"Brand": "PIRELLI",
+		"Model": "SCORPION VERDE A/S",
+		"price": "NONE",
+		"image": "images/275-45-21_PIR.jpeg"
+	},
+	{
+		"Size": "275/45R21",
+		"Brand": "PIRELLI",
+		"Model": "P-ZERO PZ4 (MO-S)",
+		"price": "NONE",
+		"image": "images/275-45-21_PIR_ZERO.jpeg"
+	},
+	{
+		"Size": "275/45R21",
+		"Brand": "ACCELERA",
+		"Model": "IOTA-ST68",
+		"price": "NONE",
+		"image": "images/275-45-21_ACC.jpeg"
+	},
+	{
+		"Size": "285/45R21",
+		"Brand": "PIRELLI",
+		"Model": "SCORPION ZERO ASIM",
+		"price": "NONE",
+		"image": "images/285-45-21_PIR.jpeg"
+	},
+	{
+		"Size": "285/45R21",
+		"Brand": "CONTINENTAL",
+		"Model": "Premium Contact 6",
+		"price": "NONE",
+		"image": "images/285-45-21_CONTI.jpeg"
+	},
+	{
+		"Size": "315/40R21",
+		"Brand": "PIRELLI",
+		"Model": "P_ZERO PZ4 (MO)",
+		"price": "NONE",
+		"image": "images/315-40-21_PIR.jpeg"
+	}
+	
+];
+
+var products = "",
+	Sizes = "",
+	Brands = "",
+	Models = "";
+
+for (var i = 0; i < data.length; i++) {
+	var Size = data[i].Size,
+		Brand = data[i].Brand,
+		Model = data[i].Model,
+		price = data[i].price,
+		rawPrice = price.replace("$",""),
+		rawPrice = parseInt(rawPrice.replace(",","")),
+		image = data[i].image;
+	
+	//create product cards
+	products += "<div class='col-sm-4 product' data-Size='" + Size + "' data-Brand='" + Brand + "' data-Model='" + Model + "' data-price='" + rawPrice + "'><div class='product-inner text-center'><img class='pp' src='" + image + "'><br />Size: " + Size + "<br />Brand: " + Brand + "<br />Model: " + Model + "<br />Price: " + price + "</div></div>";
+	
+	//create dropdown of makes
+	if (Sizes.indexOf("<option value='" + Size + "'>" + Size + "</option>") == -1) {
+		Sizes += "<option value='" + Size + "'>" + Size + "</option>";
+	}
+	
+	//create dropdown of Brands
+	if (Brands.indexOf("<option value='" + Brand+"'>" + Brand + "</option>") == -1) {
+		Brands += "<option value='" + Brand + "'>" + Brand + "</option>";
+	}
+	
+	//create dropdown of Models
+	if (Models.indexOf("<option value='" + Model + "'>" + Model + "</option>") == -1) {
+		Models += "<option value='" + Model + "'>" + Model + "</option>";
+	}
+}
+
+$("#products").html(products);
+$(".filter-Size").append(Sizes);
+$(".filter-Brand").append(Brands);
+$(".filter-Model").append(Models);
+
+var filtersObject = {};
+
+//on filter change
+$(".filter").on("change",function() {
+	var filterSize = $(this).data("filter"),
+		filterVal = $(this).val();
+	
+	if (filterVal == "") {
+		delete filtersObject[filterSize];
+	} else {
+		filtersObject[filterSize] = filterVal;
+	}
+	
+	var filters = "";
+	
+	for (var key in filtersObject) {
+	  	if (filtersObject.hasOwnProperty(key)) {
+			filters += "[data-"+key+"='"+filtersObject[key]+"']";
+	 	}
+	}
+	
+	if (filters == "") {
+		$(".product").show();
+	} else {
+		$(".product").hide();
+		$(".product").hide().filter(filters).show();
+	}
+});
+
+//on search form submit
+$("#search-form").submit(function(e) {
+	e.preventDefault();
+	var query = $("#search-form input").val().toLowerCase();
+
+	$(".product").hide();
+	$(".product").each(function() {
+		var Size = $(this).data("Size").toLowerCase(),
+			Brand = $(this).data("Brand").toLowerCase(),
+			Model = $(this).data("Model").toLowerCase();
+
+		if (Size.indexOf(query) > -1 || Brand.indexOf(query) > -1 || Model.indexOf(query) > -1) {
+			$(this).show();
+		}
+	});
+});
